eb993d4b27bfde3f3d683a202f25f4b26b982066
huangqingjian <qingjian.huang@zhangmen.com>
2023-03-19 22:19:32 +0800

    兼容chatgpt tls version

--- chatgpt-service/src/main/java/com/chatgpt/util/HttpClient.java
+++ chatgpt-service/src/main/java/com/chatgpt/util/HttpClient.java
@@ -2,6 +2,7 @@ package com.chatgpt.util;
 
 import com.chatgpt.constant.Constant;
 import com.chatgpt.exception.ServiceException;
+import org.apache.http.HttpVersion;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -10,12 +11,15 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.TrustStrategy;
 import org.apache.http.util.EntityUtils;
 import org.springframework.util.CollectionUtils;
 
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -54,12 +58,22 @@ public class HttpClient {
         CloseableHttpClient httpClient = null;
         CloseableHttpResponse response = null;
         try {
-            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
-                //信任所有
-                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                    return true;
+            SSLContext sslContext = SSLContext.getInstance("TLSV1.2");
+            X509TrustManager trustManager = new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
                 }
-            }).build();
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+            sslContext.init(null, new TrustManager[] { trustManager }, null);
             SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
             // 创建Httpclient对象
             httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
